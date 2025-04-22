8b267ec576b3def63c3226c263d105e9afc7fd00
rhysmckiernan0@gmail.com <rhysmckiernan0@gmail.com>
2025-04-22 14:44:59 +0000

    int_orders_margin created

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,9 +1,9 @@
-SELECT m.orders_id
-      ,m.revenue
-      ,m.quantity
-      ,m.purchase_cost
-      ,m.margin
-      ,s.date_date
-FROM {{ref("int_sales_margin")}} m
-JOIN {{ref("stg_raw__sales")}} s
-GROUP BY date_date
+SELECT orders_id
+      ,date_date
+      ,ROUND(SUM(revenue),2) AS revenue
+      ,ROUND(SUM(quantity),2) AS quantity
+      ,ROUND(SUM(purchase_cost),2) AS purchase_cost
+      ,ROUND(SUM(margin),2) AS margin
+FROM {{ref("int_sales_margin")}} 
+GROUP BY orders_id, date_date
+ORDER BY date_date DESC, orders_id
