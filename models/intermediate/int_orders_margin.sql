b05f290885aa830694ded5bb92b7ea4b29629804
rhysmckiernan0@gmail.com <rhysmckiernan0@gmail.com>
2025-04-22 14:32:03 +0000

    amended int_sales_margin to add date_date

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,8 +1,9 @@
-SELECT orders_id
-      ,date_date
-      ,revenue
-      ,quantity
-      ,purchase_cost
-      ,margin
-FROM int_sales_margin
+SELECT m.orders_id
+      ,m.revenue
+      ,m.quantity
+      ,m.purchase_cost
+      ,m.margin
+      ,s.date_date
+FROM {{ref("int_sales_margin")}} m
+JOIN {{ref("stg_raw__sales")}} s
 GROUP BY date_date
