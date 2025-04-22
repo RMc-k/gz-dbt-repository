19f235ee2b66c804abe5e9dd0d158208afe1423e
rhysmckiernan0@gmail.com <rhysmckiernan0@gmail.com>
2025-04-22 14:15:06 +0000

    int_sales_margin created

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,9 @@
+SELECT  sales.orders_id
+       ,sales.quantity
+       ,sales.revenue
+       ,product.purchase_price
+       ,sales.quantity * product.purchase_price AS purchase_cost
+       ,ROUND(sales.revenue - (sales.quantity * product.purchase_price), 2) AS margin
+FROM {{ ref('stg_raw__sales')}} sales
+JOIN {{ ref('stg_raw__product')}} product  
+ON sales.products_id = product.products_id
